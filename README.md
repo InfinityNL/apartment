e90eaab31c2e28ce44990334e216ea41bf147bdd
Mr.X <marco-cali@hotmail.com>
2023-03-25 09:53:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Custom InfinityRP Studio Apartment
+
+Location:
+
+vector4(-267.36, -967.36, 110.1, 162.3)
+
+
+https://gta.fandom.com/wiki/3_Alta_Street
---
+++ infinity_apartment/fxmanifest.lua
@@ -0,0 +1,7 @@
+author "Mrx29 - InfinityRP - http://www.twitch.tv/mrx29"
+description "infinity apartment (Custom InfinityRP MLO)"
+
+fx_version "cerulean"
+game "gta5"
+    
+this_is_a_map 'yes'
